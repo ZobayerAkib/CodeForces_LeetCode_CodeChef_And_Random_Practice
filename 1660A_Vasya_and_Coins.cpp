bbbd391bbfe4599c18df6fe97be2a9f50d06442f
Md. Zobayer Ibna Kabir <66842328+ZobayerAkib@users.noreply.github.com>
2022-07-04 04:21:30 +0600

    cpp_math_problem_practicing

---
+++ 1660A_Vasya_and_Coins.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+      int a , b ;
+    cin>>a>>b;
+    if(a>0)
+    {
+        cout<<((a*1)+(b*2)+1)<<endl;
+    }
+    if(a==0)
+    {
+        cout<<1<<endl;
+    }
+
+    }
+
+    }
