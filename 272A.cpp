fba19048562d1a4dd955574bc27a15576bbae3be
Md. Zobayer Ibna Kabir <66842328+ZobayerAkib@users.noreply.github.com>
2022-08-31 14:47:32 +0600

    solving number theory related que

---
+++ 272A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int n;
+    cin>>n;
+    if(n%2==0)
+    {
+        cout<<n-8<<" "<<8<<endl;
+    
+    }
+    else
+    {
+        cout<<n-9<<" "<<9<<endl;
+    }
+
+
+}
