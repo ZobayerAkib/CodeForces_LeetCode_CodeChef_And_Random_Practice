b6c6ff30bdf85773a1f867bb36f7fce62ddd90e7
Md. Zobayer Ibna Kabir <66842328+ZobayerAkib@users.noreply.github.com>
2023-01-03 16:56:21 +0600

    Add files via upload

---
+++ 1740A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void solve()
+{
+    int n,m=0;
+    cin>>n;
+    m=n;
+    cout<<m<<endl;
+}
+
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)solve();
+}
