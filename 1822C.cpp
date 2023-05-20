5b1a6be2ffc434b77098014b20e81e03ec65d345
Md. Zobayer Ibna Kabir <66842328+ZobayerAkib@users.noreply.github.com>
2023-05-21 01:36:04 +0600

    Add files via upload

---
+++ 1822C.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#define ll long long int
+void solve()
+{
+    ll n,n1;
+    cin>>n;
+    n1=n+1;
+    cout<<(n1*n1)+1<<endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) solve();
+        
+}
