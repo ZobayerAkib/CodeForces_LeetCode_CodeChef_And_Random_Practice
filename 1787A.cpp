6f140917ac6c480429349e8adb61c13367a35f83
Md. Zobayer Ibna Kabir <66842328+ZobayerAkib@users.noreply.github.com>
2023-05-26 14:27:39 +0600

    Add files via upload

---
+++ 1787A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+   
+    
+    int n;
+    cin>>n;
+    if(n%2==0) cout<<1<<' '<<n/2<<endl;
+    else cout<<"-1"<<endl;
+
+}
+
+int32_t main(){
+ ios_base::sync_with_stdio(0);
+cin.tie(0);
+int n;cin>>n;
+while(n--) solve();
+
+}
